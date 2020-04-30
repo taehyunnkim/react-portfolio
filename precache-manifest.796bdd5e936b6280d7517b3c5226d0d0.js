2bd61f9e4a024f1617f9e44cfd647f7b9524ef26
Eric Kim <kim2000@uw.edu>
2020-04-30 12:59:32 +0900

    Updates

---
+++ precache-manifest.796bdd5e936b6280d7517b3c5226d0d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad9f31d255f1ff986bf11c7fe774c79",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b2313733ec13f8b68e57",
+    "url": "/react-portfolio/static/css/main.74d4670d.chunk.css"
+  },
+  {
+    "revision": "9ff033849703de4bee04",
+    "url": "/react-portfolio/static/js/2.9d4cb79a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.9d4cb79a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2313733ec13f8b68e57",
+    "url": "/react-portfolio/static/js/main.cc152475.chunk.js"
+  },
+  {
+    "revision": "0938b211af4f266a20b0",
+    "url": "/react-portfolio/static/js/runtime-main.0319e79d.js"
+  }
+]);
